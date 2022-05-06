77da253cda236b3af6c03bcd16580d5895776a5a
GenoSamuel <edwin.grigonis2@mail.dcu.ie>
2022-05-06 14:20:02 +0900

    made changes to api call, still broken

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,5 +1,6 @@
 import {useEffect, useState} from 'react'
 import Spinner from '../layout/Spinner'
+import UserItem from './UserItem'
 
 function UserResults() {
     const [users, setUsers] = useState([])
@@ -7,15 +8,11 @@ function UserResults() {
 
     useEffect(() => {
         fetchUsers()
-    }, [])
+    }, [users])
 
 const fetchUsers = async () => {
-    const response = await fetch(`${process.env.REACT_APP_GITHUB_API}/users`, {
-    headers: {
-        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
-    }
-    })
-
+    const response = await fetch(`https://api.github.com/users`)
+    console.log(response)
     const data = await response.json()
    
     setUsers(data)
@@ -26,7 +23,7 @@ const fetchUsers = async () => {
     <div className='grid grid-cols-1 gap-8 xl:grid-cols-4
         lg:grid-cols-3 md:grid-cols-2'>
         {users.map((user) => (
-            <h3>{user.login}</h3>
+            <UserItem key={user.id} user={user} />
         ))}
     </div>
   )
