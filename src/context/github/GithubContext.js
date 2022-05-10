16779469e52611d868fd7c194faf189d535278d4
GenoSamuel <edwin.grigonis2@mail.dcu.ie>
2022-05-10 16:10:55 +0900

    introduced search

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -16,18 +16,22 @@ export const GithubProvider = ({children}) => {
 
 //get initial users plus testing
 
-    const fetchUsers = async () => {
+    const searchUsers = async (text) => {
         setLoading()
-        const response = await fetch(`${GITHUB_URL}/users`/*, {
+
+        const params = new URLSearchParams({
+            q: text
+        })
+        const response = await fetch(`${GITHUB_URL}/search/users?${params}`/*, {
             headers: {
                 Authorization: `token ${GITHUB_TOKEN}`,
             }
         }*/)
-        const data = await response.json()
+        const {items} = await response.json()
        
         dispatch({
             type: 'GET_USERS',
-            payload: data,
+            payload: items,
         })
 
     }
@@ -39,7 +43,7 @@ export const GithubProvider = ({children}) => {
     return <GithubContext.Provider value= {{
         users: state.users,
         loading: state.loading,
-        fetchUsers
+        searchUsers
     }}>
     {children}
     </GithubContext.Provider>
