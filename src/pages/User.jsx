caba3640993d80bc78748afb2f6b4f2ef54a907e
GenoSamuel <edwin.grigonis2@mail.dcu.ie>
2022-05-13 10:01:06 +0900

    added profile pic

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -41,6 +41,15 @@ function User() {
             <div className='mb-4 '>
                 <Link to={'/'} className='btn btn-ghost'>Back to search</Link>
             </div>
+            <div className='grid grid-cols-1 xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-3 mb-8 md:gap-8'>
+                <div className='custom-card-image mb-6 md:mb-0'>
+                    <div className='rounded-lg shadow-xl card image-full'>
+                        <figure>
+                            <img src={avatar_url} alt="profile pic" />
+                        </figure>
+                    </div>
+                </div>
+            </div>
         </div>
     </>
   )
